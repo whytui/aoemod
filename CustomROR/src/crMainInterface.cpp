ed76598a19cc66aec797ffaed4d7a8ca02bec504
AOEmodder <aoemodder@aol.com>
2016-09-25 20:26:48 +0200

    debug test: force unit group to defend a unit, using AI mechanics

--- CustomROR/src/crMainInterface.cpp
+++ CustomROR/src/crMainInterface.cpp
@@ -187,10 +187,48 @@ bool CustomRORMainInterface::GameAndEditor_OnKeyPress(long int pressedKey, bool 
 		
 		//TEST exp
 		AOE_STRUCTURES::STRUCT_PLAYER *player = GetControlledPlayerStruct_Settings();
-		CR_DEBUG::exportInfAIExplorationToBitmap(player);
-		CR_DEBUG::exportGameTerrainRestrictionValuesToBitmap();
+		//CR_DEBUG::exportInfAIExplorationToBitmap(player);
+		//CR_DEBUG::exportGameTerrainRestrictionValuesToBitmap();
 		/*_BITMAP::BitmapExporter::ExportDataAsBitmapGreyShades("D:\\test1.bmp", global->gameMapInfo->mapArraySizeX, 
 			global->gameMapInfo->mapArraySizeY, global->gameMapInfo->unknown_8DB0, 0, 255, false);*/
+		if (player->ptrAIStruct && player->ptrAIStruct->IsCheckSumValid()) {
+			AOE_STRUCTURES::STRUCT_UNIT_GROUP_ELEM *fakeGroup = &player->ptrAIStruct->structTacAI.fakeFirstUnitGroupElem;
+			AOE_STRUCTURES::STRUCT_UNIT_GROUP_ELEM *curGroup = fakeGroup->next;
+			while (curGroup && (curGroup != fakeGroup)) {
+				if ((curGroup->unitGroupType == UNIT_GROUP_TYPES::CST_UGT_LAND_ATTACK) && (curGroup->commanderUnitId >= 0)) {
+					AOE_STRUCTURES::STRUCT_UNIT_BASE *unitWithShortcut1 = FindUnitWithShortcutNumberForPlayer(player, 1);
+					if (unitWithShortcut1 && unitWithShortcut1->IsCheckSumValidForAUnitClass()) {
+						curGroup->taskSubTypeId = -1;
+						curGroup->currentTask = UNIT_GROUP_TASK_IDS::CST_UGT_DEFEND_UNIT;
+						curGroup->targetPlayerId = player->playerId;
+						curGroup->targetUnitId = unitWithShortcut1->unitInstanceId;
+						curGroup->targetPosX = unitWithShortcut1->positionX;
+						curGroup->targetPosY = unitWithShortcut1->positionY;
+						AOE_STRUCTURES::STRUCT_UNITDEF_BASE *unitDef = unitWithShortcut1->unitDefinition;
+						curGroup->targetDAT_ID = -1;
+						if (unitDef && unitDef->IsCheckSumValidForAUnitClass()) {
+							curGroup->targetDAT_ID = unitDef->DAT_ID1;
+						}
+						curGroup->unknown_resetOrg = 1;
+						unsigned long int addr = 0x4CD2D0;
+						AOE_STRUCTURES::STRUCT_AI *mainAI = player->ptrAIStruct;
+						AOE_STRUCTURES::STRUCT_TAC_AI *tacAI = &player->ptrAIStruct->structTacAI;
+						long int taskId = (long int)UNIT_GROUP_TASK_IDS::CST_UGT_DEFEND_UNIT;
+						_asm {
+							MOV ECX, curGroup;
+							PUSH 0;
+							PUSH 0;
+							PUSH taskId;
+							PUSH mainAI;
+							PUSH tacAI;
+							CALL addr;
+						}
+					}
+				}
+				curGroup = curGroup->next;
+			}
+			
+		}
 	}
 
 	// TEST - F8 - show dialog
